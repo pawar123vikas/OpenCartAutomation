b2f05b0d0b8a4267c7903a7507c65ad676957a00
vikas <pawar123vikas@gmail.com>
2023-12-19 14:21:51 +0530

    update cart page as review per comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -4,7 +4,8 @@ import org.openqa.selenium.By;
 
 public class CartPage {
 	
-	
+	private By cart = By.id("cart");
+	private By page = By.id("page");
 
 	public void getCart() {
 		System.out.println("cart is added");
